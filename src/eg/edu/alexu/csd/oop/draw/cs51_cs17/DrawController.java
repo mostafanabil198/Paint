53d07daae02a4be687fe603eaa6c591dbd5cb8fa
reham <rehamnaguib1@gmail.com>
2018-11-03 03:07:04 +0200

    undo and redo

--- src/eg/edu/alexu/csd/oop/draw/cs51_cs17/DrawController.java
+++ src/eg/edu/alexu/csd/oop/draw/cs51_cs17/DrawController.java
@@ -3,12 +3,17 @@ package eg.edu.alexu.csd.oop.draw.cs51_cs17;
 import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import eg.edu.alexu.csd.oop.draw.DrawingEngine;
 import eg.edu.alexu.csd.oop.draw.Shape;
 
 public class DrawController implements DrawingEngine {
 	ArrayList<Shape> shapes = new ArrayList<Shape>();
+	Stack<ArrayList<Shape>> undoStack=new Stack<>();
+	Stack<ArrayList<Shape>> redoStack=new Stack<>();
+	ArrayList<Shape> currentImage=new ArrayList<Shape>();
+	ArrayList<Shape> temp=new ArrayList<Shape>();
 	@Override
 	public void refresh(Graphics canvas) {
 		// TODO Auto-generated method stub
@@ -17,20 +22,25 @@ public class DrawController implements DrawingEngine {
 
 	@Override
 	public void addShape(Shape shape) {
+		undoStack.push(shapes);
 		shapes.add(shape);
+		undoStack.push(shapes);
 
 	}
 
 	@Override
 	public void removeShape(Shape shape) {
+		undoStack.push(shapes);
 		shapes.remove(shape);
+		undoStack.push(shapes);
 	}
 
 	@Override
 	public void updateShape(Shape oldShape, Shape newShape) {
-
+		undoStack.push(shapes);
 		shapes.remove(oldShape);
 		shapes.add(newShape);
+		undoStack.push(shapes);
 	}
 
 	@Override
@@ -48,13 +58,15 @@ public class DrawController implements DrawingEngine {
 	@Override
 	public void undo() {
 		// TODO Auto-generated method stub
-
+      temp=undoStack.pop();
+      redoStack.push(temp);
+      currentImage=undoStack.peek();
 	}
 
 	@Override
 	public void redo() {
-		// TODO Auto-generated method stub
-
+		currentImage=redoStack.pop();
+		undoStack.push(currentImage);
 	}
 
 	@Override
---
+++ src/eg/edu/alexu/csd/oop/draw/cs51_cs17/GUI/Paint.java
@@ -0,0 +1,42 @@
+package eg.edu.alexu.csd.oop.draw.cs51_cs17.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class Paint extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Paint frame = new Paint();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Paint() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
