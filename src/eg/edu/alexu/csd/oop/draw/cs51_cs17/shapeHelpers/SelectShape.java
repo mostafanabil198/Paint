8196db65b77eba9b560ce9386226dde93eac5eb6
reham <rehamnaguib1@gmail.com>
2018-11-15 21:02:32 +0200

    changes

--- src/eg/edu/alexu/csd/oop/draw/cs51_cs17/shapeHelpers/SelectShape.java
+++ src/eg/edu/alexu/csd/oop/draw/cs51_cs17/shapeHelpers/SelectShape.java
@@ -1,10 +1,7 @@
 package eg.edu.alexu.csd.oop.draw.cs51_cs17.shapeHelpers;
 
-<<<<<<< HEAD
 import java.util.Map;
 
-=======
->>>>>>> bbc26d69fb73b83499d7752f70178d7b22f4ce39
 import eg.edu.alexu.csd.oop.draw.Shape;
 
 public class SelectShape {
@@ -14,7 +11,6 @@ public class SelectShape {
 		for (int i = shapes.length - 1; i >= 0; i--) {
 			switch (shapes[i].getClass().getSimpleName()) {
 			case "Circle":
-<<<<<<< HEAD
 				shape = selectCircle(shapes[i], x, y);
 				break;
 			case "Ellipse":
@@ -31,24 +27,6 @@ public class SelectShape {
 				break;
 			case "Rectangle":
 				shape = selectRectangle(shapes[i], x, y);
-=======
-				shape = selectCircle();
-				break;
-			case "Ellipse":
-				shape = selectEllipse();
-				break;
-			case "Line":
-				shape = selectLine();
-				break;
-			case "Triangle":
-				shape = selectTriangle();
-				break;
-			case "Square":
-				shape = selectSquare();
-				break;
-			case "Rectangle":
-				shape = selectRectangle();
->>>>>>> bbc26d69fb73b83499d7752f70178d7b22f4ce39
 				break;
 			}
 			if (shape != null) {
@@ -58,18 +36,15 @@ public class SelectShape {
 		return shape;
 	}
 
-<<<<<<< HEAD
 	private Shape selectCircle(Shape s, float x, float y) {
 		double topX=s.getProperties().get("centerX");
 		double topY=s.getProperties().get("centerY");
 		double radius=s.getProperties().get("width");
-		double centerX=(topX+radius)/2;
-		double centerY=(topY+radius)/2;
-		double reqx=Math.abs(x-centerX);
-		double reqy=Math.abs(y-centerY);
-		double lenx=Math.abs(topX-centerX);
-		double leny=Math.abs(topY-centerY);
-		if(Math.sqrt(reqx*reqx+reqy*reqy)<=Math.sqrt(lenx*lenx+leny*leny)) {
+		double centerX=(topX+radius);
+		double centerY=(topY-radius);
+		double diffx=Math.abs(x-centerX);
+		double diffy=Math.abs(y-centerY);
+		if(diffx*diffx+diffy*diffy<=radius*radius) {
 			return s;
 		}
 		return null;
@@ -80,12 +55,29 @@ public class SelectShape {
 		double topY=s.getProperties().get("centerY");
 		double radiusA=s.getProperties().get("width");
 		double radiusB=s.getProperties().get("height");
-		
+		double centerX=topX+radiusA/2;
+		double centerY=topY-radiusB/2;
+		double diffX=Math.abs(x-centerX);
+		double diffY=Math.abs(y-centerY);
+		if((diffX*diffX/radiusA*radiusA+diffY*diffY/radiusB*radiusB)<=1) {
+			return s;
+		}
 		return null;
 	}
 
 	private Shape selectLine(Shape s, float x, float y) {
-		return null;
+		double sPointX=s.getProperties().get("point1X");
+		double sPointY=s.getProperties().get("point1Y");
+		double endPointX=s.getProperties().get("point2X");
+		double endPointY=s.getProperties().get("point2Y");
+		double firstRatio=x-sPointX/y-endPointX;
+		double secondRatio=endPointX-sPointX/endPointY-sPointY;
+		if(firstRatio==secondRatio) {
+			return s;
+		}
+		else {
+			return null;
+		}
 	}
 
 	private Shape selectTriangle(Shape s, float x, float y) {
@@ -98,29 +90,6 @@ public class SelectShape {
 	}
 
 	private Shape selectRectangle(Shape s, float x, float y) {
-=======
-	private Shape selectCircle() {
-		return null;
-	}
-
-	private Shape selectEllipse() {
-		return null;
-	}
-
-	private Shape selectLine() {
-		return null;
-	}
-
-	private Shape selectTriangle() {
-		return null;
-	}
-
-	private Shape selectSquare() {
-		return null;
-	}
-
-	private Shape selectRectangle() {
->>>>>>> bbc26d69fb73b83499d7752f70178d7b22f4ce39
 		return null;
 	}
 
